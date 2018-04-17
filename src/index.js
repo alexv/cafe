a2d362dc9784b29fc1eac8492f0377a2c7e53006
Alex Villarreal <avillarreal01@gmail.com>
2018-04-17 08:38:58 -0400

    add tag

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
 
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import { deepOrange700 } from 'material-ui/styles/colors'
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: deepOrange700
+  }
+})
 
 ReactDOM.render(
-  <MuiThemeProvider>
+  <MuiThemeProvider muiTheme={muiTheme}>
     <Router>
       <App />
     </Router>
